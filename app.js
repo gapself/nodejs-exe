832c5be27ad36abcd21904295c6df6f8b44d0ab3
gapself <gabriela.paszkiewicz@gmail.com>
2018-09-04 22:47:17 +0300

    Using 3rd Party Modules

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ console.log('Starting app.js');
 const fs = require('fs');
 //i wanna fetch fs module in fs file
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
+
+//lowdash 1)you dont have to keep rewriting methods 2)well-tested, tried in production
+//_.isString
+console.log(_.isString(true)); //==> false-> (cause boolean's not a string
+console.log(_.isString('Andrew')); //true comes up cause Andrew is str
+
+//_.uniq
+var filteredArray = _.uniq(['Andrew',1,'Andrew',1,2,3,4])
+console.log(filteredArray); //[ 'Andrew', 1, 2, 3, 4 ]
 
 //1st version
 var res = notes.add(1,2);
