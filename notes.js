ead89c58fe30ad26d1ffb7d62d2773ea97c743ba
gapself <gabriela.paszkiewicz@gmail.com>
2018-09-06 14:24:30 +0200

    Adding & saving notes

--- notes.js
+++ notes.js
@@ -1,9 +1,28 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
 //addNote function which's in app.js
 //node app.js add --title=secrets --body="This is my secret"
 var addNote = (title,body) => {
-    console.log('Adding note',title,body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+//  loop through all of the notes and check if there're any new notes, if there're duplicates we're not gonna call 2 lines: if there arent than;;; notes.push(), etc if we run file that exists - catch'll never work
+// code that loads the file
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+    }
+//return true-keep that array inside
+// code that saves the file
+    var duplicatesNotes = notes.filter((note) => note.title === title); //return note.title...
+    if (duplicatesNotes.length === 0 ) {
+        notes.push(note);  //if re = to 0 then we wanna push notes
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 var getAll = () => {
     console.log('Getting all notes');
@@ -21,7 +40,6 @@ module.exports={
     getNote,
     removeNote,
 };
-
 
 //add
 module.exports.add = (a,b) => {
